2fc18daf53c30c4a5f95d8c5c654b5723b28e920
jceferino-2022238 <jceferino-2022238@kinal.edu.gt>
2024-04-29 17:27:26 -0600

    Se continuo con la estructura del FormAdd

---
+++ src/components/FormAdd/FormAdd.jsx
@@ -0,0 +1,118 @@
+import { useState } from "react"
+import DatePicker from "react-date-picker"
+import { Input } from "../Input"
+const inputs = [
+    {
+        field: "nombreTarea",
+        label: "NombreTarea",
+        validationMessage: "",
+        type: "text"
+    },
+    {
+        field: "descripcionTarea",
+        label: "DescripcionTarea",
+        validationMessage: "",
+        type: "text"
+    },
+    {
+        field: "empleadoAsignado",
+        label: "EmpleadoAsignado",
+        validationMessage: "",
+        type: "text"
+    }
+]
+export const FormAdd = ({ settings, saveSettings }) => {
+    const [formState, setFormState] = useState({
+        nombreTarea: {
+            isValid: "",
+            showError: false,
+            value: settings.nombreTarea
+        },
+        descripcionTarea: {
+            isValid: "",
+            showError: false,
+            value: settings.descripcionTarea
+        },
+        empleadoAsignado: {
+            isValid: "",
+            showError: false,
+            value: settings.nombreTarea
+        },
+
+    })
+const handleInputValueChange = (value, field) => {
+    setFormState((prevState) => ({
+         ...prevState,
+        [field]: {
+        ...prevState[field],
+        value,
+        }
+    }))
+}
+const handleInputValidationOnBlur = (value, field) =>{
+    let isValid = false;
+    switch (field) {
+        case "nombreTarea":
+            isValid="";
+            break;
+        case "descripcionTarea":
+            break;
+        case "empleadoAsignado":
+            break;
+        default:
+            break;
+    }
+    setFormState((prevState) =>({
+        ...prevState,
+        [field]:{
+            ...prevState[field],
+            isValid,
+            showError: !isValid
+        }
+    }))
+};
+const handleFormSubmit = (event) =>{
+    event.preventDefault()
+    saveSettings({
+        nombreTarea: formState.nombreTarea.value,
+        descripcionTarea: formState.descripcionTarea.value,
+        empleadoAsignado: formState.empleadoAsignado.value
+    })
+
+    console.log(formState)
+}
+
+const isSubmitButtonDisabled = !formState.nombreTarea.isValid ||
+                               !formState.descripcionTarea.isValid ||
+                               !formState.empleadoAsignado.isValid
+return (
+    <form>
+        <div>
+            {inputs.map((input) =>(
+                <Input
+                    key={input.field}
+                    field={input.field}
+                    label={input.label}
+                    value={formState[input.field].value}
+                    onChangeHandler={handleInputValueChange}
+                    onBlurHandler={handleInputValidationOnBlur}
+                    showErrorMessage={formState[input.field].showError}
+                    validationMessage={input.validationMessage}
+                    type={input.type}
+                    textArea={input.textArea}
+                />
+            ))}
+            <DatePicker/>
+            <DatePicker/>
+        </div>
+        <div>
+            <button onClick={handleFormSubmit} disabled={isSubmitButtonDisabled}>
+                Guardar
+            </button>
+            <button>
+                Cancelar
+            </button>
+        </div>
+    </form>
+)
+}
---
+++ src/pages/dashboard/DashboardPage.jsx
@@ -0,0 +1,9 @@
+import 
+import './dashBoardPage.css'
+export const DashboardPage = () =>{
+    return(
+        <div>
+            <FormAdd/>
+        </div>
+    )
+}
