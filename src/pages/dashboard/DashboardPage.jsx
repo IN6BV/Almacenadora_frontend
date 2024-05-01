0ae9bde88f4f344d431dc2124e24579a42293c98
dmorales2022171 <dmorales-2022171@kinal.edu.gt>
2024-04-30 22:17:52 -0600

    se hizo el formulario de editar

--- src/pages/dashboard/DashboardPage.jsx
+++ src/pages/dashboard/DashboardPage.jsx
@@ -1,12 +1,13 @@
-import { FormAdd } from '../../components/formAdd/FormAdd.jsx'
+import { FormAdd } from '../../components/FormAdd/FormAdd'
+import { FormEdit } from '../../components/formEdit/FormEdit'
 import { Navbar } from '../../components/navbar/Navbar'
-import { useEffect } from 'react'
 import './dashboardPage.css'
 export const DashboardPage = () => {
     return (
         <div>
             <Navbar/>
             <FormAdd/>
+            <FormEdit/>
         </div>
     )
 }
