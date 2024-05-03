36936dac8e468dc9f2b7fbf173044e2fb222701d
echamale <echamale-2022222@kinal.edu.gt>
2024-05-03 02:33:31 -0600

    Limpiando dashboard

--- src/pages/dashboard/DashboardPage.jsx
+++ src/pages/dashboard/DashboardPage.jsx
@@ -1,13 +1,8 @@
-import { FormAdd } from '../../components/FormAdd/FormAdd'
-import { FormEdit } from '../../components/formEdit/FormEdit'
-import { Navbar } from '../../components/navbar/Navbar'
 import './dashboardPage.css'
 export const DashboardPage = () => {
     return (
         <div>
-            <Navbar/>
-            <FormAdd/>
-            <FormEdit/>
+            
         </div>
     )
 }
